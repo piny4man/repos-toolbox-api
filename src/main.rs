8d9bd49b35d0693d432af76ab98bba0d864ecff8
Alberto Mendez <development@pinya.dev>
2023-05-01 21:45:10 +0200

    Fix/repo endpoint (#5)
    * Add github repo search + Languages fetch and return all data together
    
    * Fetch languages also on repository search
    
    * Fix linting

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use std::collections::HashMap;
+
 use axum::{
     extract::Query,
     http::{self, StatusCode},
-    routing::get,
+    routing::{get, post},
     Json, Router,
 };
 use axum_macros::debug_handler;
 use octocrab::{models::Repository, Page};
+use reqwest::{Client, Url};
 use serde::{Deserialize, Serialize};
 use tower_http::cors::{Any, CorsLayer};
 
@@ -20,7 +23,13 @@ struct RepoRequest {
     repo: String,
 }
 
-#[derive(Serialize)]
+#[derive(Debug, Serialize)]
+struct RepoResponse {
+    repo: Repository,
+    languages: HashMap<String, u64>,
+}
+
+#[derive(Debug, Serialize)]
 struct RepositoryPage {
     items: Vec<Repository>,
     #[serde(rename = "total_count")]
@@ -38,10 +47,37 @@ impl From<Page<Repository>> for RepositoryPage {
     }
 }
 
+async fn get_repository_languages(url: Url) -> Result<HashMap<String, u64>, (StatusCode, String)> {
+    let response = Client::new()
+        .get(url)
+        .header("User-Agent", "repos-toolbox-api")
+        .send()
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    if !response.status().is_success() {
+        let error_message = format!(
+            "Error fetching language data. Status code: {}",
+            response.status()
+        );
+        return Err((StatusCode::INTERNAL_SERVER_ERROR, error_message));
+    }
+
+    let response_text = response
+        .text()
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    let languages: HashMap<String, u64> = serde_json::from_str(&response_text)
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    Ok(languages)
+}
+
 #[debug_handler]
 async fn search_repository(
     Query(params): Query<SearchParams>,
-) -> Result<Json<RepositoryPage>, (StatusCode, String)> {
+) -> Result<Json<Vec<RepoResponse>>, (StatusCode, String)> {
     let page = octocrab::instance()
         .search()
         .repositories(&params.repo)
@@ -49,20 +85,37 @@ async fn search_repository(
         .await
         .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
-    Ok(Json(RepositoryPage::from(page)))
+    let mut repos_response = Vec::new();
+
+    for repo in page.items {
+        let url = repo.clone().languages_url.unwrap();
+        let repo_languages = get_repository_languages(url).await;
+        match repo_languages {
+            Ok(languages) => repos_response.push(RepoResponse { repo, languages }),
+            Err(e) => {
+                tracing::error!("Error fetching languages for repo: {}", e.1);
+            }
+        }
+    }
+
+    Ok(Json(repos_response))
 }
 
+// TODO Simplify code
 #[debug_handler]
 async fn get_repository(
     Json(payload): Json<RepoRequest>,
-) -> Result<Json<Repository>, (StatusCode, String)> {
+) -> Result<Json<RepoResponse>, (StatusCode, String)> {
     let repo = octocrab::instance()
         .repos(payload.owner, payload.repo)
         .get()
         .await
         .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
-    Ok(Json(repo))
+    let url = repo.clone().languages_url.unwrap();
+    let languages: HashMap<String, u64> = get_repository_languages(url).await?;
+
+    Ok(Json(RepoResponse { repo, languages }))
 }
 
 #[shuttle_runtime::main]
@@ -74,7 +127,7 @@ async fn axum() -> shuttle_axum::ShuttleAxum {
 
     let router = Router::new()
         .route("/search", get(search_repository))
-        .route("/repo", get(get_repository))
+        .route("/repo", post(get_repository))
         .layer(cors);
 
     tracing::info!("Starting server");
